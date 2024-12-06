953ca845a60c7705a519137fa48949516d9cabdf
Miguel A. Murillo <mgamj13@gmail.com>
2024-12-05 22:49:59 -0700

    apis

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,21 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+use App\Http\Controllers\AuthController;
+
+Route::prefix('auth')->group(function () {
+    Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
+    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum')->name('auth.logout');
+});
+
+use App\Http\Controllers\NoteController;
+
+Route::middleware('auth:sanctum')->prefix('notes')->group(function () {
+    Route::get('/', [NoteController::class, 'index'])->name('notes.index');          // Listar todas las notas
+    //Route::get('/', [NoteController::class, 'index'])->name('notes.index');          // Listar todas las notas
+    Route::post('/', [NoteController::class, 'store'])->name('notes.store');         // Crear una nueva nota
+    Route::get('/{note}', [NoteController::class, 'show'])->name('notes.show');      // Mostrar una nota específica
+    Route::put('/{note}', [NoteController::class, 'update'])->name('notes.update');  // Actualizar una nota específica
+    Route::delete('/{note}', [NoteController::class, 'destroy'])->name('notes.destroy'); // Eliminar una nota específica
+});
